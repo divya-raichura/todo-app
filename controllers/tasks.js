a12f9061f3fb1bce8b2de31f5ac885916630a619
Divya Raichura <divyaraichura8@gmail.com>
2022-10-30 23:21:40 +0530

    Update tasks.js

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,28 +1,25 @@
 const Task = require("../models/tasks");
+const asyncWrapper = require("./async");
 
-const createTask = async (req, res) => {
-  try {
+const createTask = asyncWrapper (async (req, res) => {
+   
     // why try catch?
     // if body does not follow above validations, error is thrown, we manage it in controllers file
     // so eg, name must be provided in body, cause we mentioned it in schema, if it is not provided, error is thrown, we manage that error here in catch block
     const task = await Task.create(req.body);
     res.status(201).json({ task });
-  } catch (err) {
-    res.status(500).json({ msg: err });
-  }
-};
+   
+});
 
-const getAllTasks = async (req, res) => {
-  try {
+const getAllTasks = asyncWrapper(async (req, res) => {
+  
     const tasks = await Task.find({});
     res.status(200).json({ tasks });
-  } catch (err) {
-    res.status(500).json({ msg: err });
-  }
-};
+   
+});
 
-const getSingleTask = async (req, res) => {
-  try {
+const getSingleTask = asyncWrapper(async (req, res) => {
+   
     // why two error responses?
     // because if id is of correct type, correct no of digits and we cannot find it in db, then 404
     // but if id is of wrong type or no of digits is incorrect then 500 error
@@ -32,13 +29,11 @@ const getSingleTask = async (req, res) => {
       return res.status(404).json({ msg: `no task with id : ${id}` });
     }
     res.status(200).json({ task });
-  } catch (err) {
-    res.status(500).json({ msg: err });
-  }
-};
+  
+});
 
-const updateTask = async (req, res) => {
-  try {
+const updateTask = asyncWrapper(async (req, res) => {
+   
     const { id } = req.params;
     const task = await Task.findOneAndUpdate({ _id: id }, req.body, {
       new: true,
@@ -48,23 +43,19 @@ const updateTask = async (req, res) => {
       return res.status(404).json({ msg: `no task with id : ${id}` });
     }
     res.status(200).json({ task });
-  } catch (err) {
-    res.status(500).json({ msg: err });
-  }
-};
+   
+});
 
-const deleteTask = async (req, res) => {
-  try {
+const deleteTask = asyncWrapper(async (req, res) => {
+   
     const { id } = req.params;
     const task = await Task.findOneAndDelete({ _id: id });
     if (!task) {
       return res.status(404).json({ msg: `no task with id : ${id}` });
     }
     res.status(200).json({ task });
-  } catch (err) {
-    res.status(500).json({ msg: err });
-  }
-};
+   
+});
 
 module.exports = {
   getAllTasks,
